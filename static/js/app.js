5c527084a2d543171236c3278d073f59bef53293
HCornick <HCornick@users.noreply.github.com>
2019-03-08 17:21:38 -0500

    Add multiple filters

--- static/js/app.js
+++ static/js/app.js
@@ -11,36 +11,61 @@ submit.on("click", function() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
+    // Level 1 attempt commented below
     // Select the input element and get the raw HTML code
-    var inputElement = d3.select("#datetime");
+    // var inputElement = d3.select("#datetime");
 
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    // var inputValue = inputElement.property("value");
 
-    console.log(inputValue);
+    // console.log(inputValue);
 
-    // Similarly, get input element and value for each other filter
-    // var citySearch = d3.select("#cityfilter");
-    // var cityVal = citySearch.property("value");
+    // Updated level 2 below
+    // Create dictionary to hold multiple search criteria inputs
+    var inputDict = {}
 
-    // var stateSearch =d3.select("#statefilter");
-    // var stateVal = stateSearch.property("value");
+    // Put criteria into dictionary, if each input exists
+    if(d3.select("#datetime").property("value")) {
+        inputDict['datetime'] = [d3.select("#datetime").property("value")];
+    }
+    if(d3.select("#cityfilter").property("value")) {
+        inputDict['city'] = [d3.select("#cityfilter").property("value")];
+    }
+    if(d3.select("#statefilter").property("value")) {
+        inputDict['state'] = [d3.select("#statefilter").property("value")];
+    }
+    if(d3.select("#countryfilter").property("value")) {
+        inputDict['country'] = [d3.select("#countryfilter").property("value")];
+    }
+    if(d3.select("#shapefilter").property("value")) {
+        inputDict['shape'] = [d3.select("#shapefilter").property("value")];
+    }
 
-    // var countrySearch = d3.select("countryfilter");
-    // var countryVal = countrySearch.property("value");
+    // View contents of inputDict
+    console.log(inputDict)
 
-    // var shapeSearch =d3.select("shapefilter");
-    // var shapeVal = shapeSearch.property("value");
+    // Define function to search on contents of inputDict
+    function inputSearch(dataSet, filters) {
+        const filterKeys = Object.keys(filters);
+        //filter only elements passing all criteria
+        return dataSet.filter((sighting) => {
+          return filterKeys.every(key => {
+            return filters[key].includes(sighting[key]);
+          })
+        })
+      }
 
-    //console.log(cityVal);
-    //console.log(stateVal);
-    //console.log(countryVal);
-    //console.log(shapeVal);
-
-    // Filter data based on user input value
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // Filter data based on user input values
+    // First attempt commented below
+    // var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
     
+    var filteredData = inputSearch(tableData, inputDict);
     console.log(filteredData);
+
+    // Remove existing table, if there is one
+    // Replace the table body in order to append new search information
+    d3.select("#filteredTable").remove();
+    d3.select("#ufo-table").append("tbody").attr("id","filteredTable");
 
     // Build table based on filteredData
     var tbody = d3.select("tbody");
